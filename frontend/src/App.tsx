9c418083e021c67e3d5e8ac28b08890d1a7bc14d
Omkar Vaity <vaity.o@northeastern.edu>
2024-10-27 23:54:12 -0400

    Update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import MetricsSection from './components/MetricsSection';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PricesPage from './components/PricesPage';
-import About from './components/About';
+
 
 const App: React.FC = () => {
   return (
